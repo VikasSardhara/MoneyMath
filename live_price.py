0e76be4fe26de804f30ab94304fedc14fd2cac3c
VikasSardhara <75750319+VikasSardhara@users.noreply.github.com>
2024-03-05 00:10:50 -0800

    Update live_price.py

--- live_price.py
+++ live_price.py
@@ -17,6 +17,6 @@ def get_live_stock_prices(stock_symbols):
         time.sleep(2)  # Refresh every 2 seconds
 
 if __name__ == "__main__":
-    stock_symbols = input("Enter comma-separated stock symbols (e.g., AMZN,GOOGL): ").upper().split(',')
+    stock_symbols = input("Enter stock symbols: ").upper()
     
     get_live_stock_prices(stock_symbols)
